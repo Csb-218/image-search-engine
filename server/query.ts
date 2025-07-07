92e344612b066234c24b4fb4d2665c77eb9c4ff8
Csb-218 <96022278+Csb-218@users.noreply.github.com>
2025-07-07 15:53:26 +0530

    chore : code refactored

--- server/query.ts
+++ server/query.ts
@@ -1,13 +1,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { unlink } from 'fs/promises';
-import { Pinecone } from '@pinecone-database/pinecone';
+import { unlink } from "fs/promises";
+import { Pinecone } from "@pinecone-database/pinecone";
 import { embedder } from "./embeddings.ts";
 import { getEnv } from "./utils/util.ts";
 
-
 export type Metadata = {
   imagePath: string;
-}
+};
 
 const indexName = getEnv("PINECONE_INDEX");
 const pinecone = new Pinecone();
@@ -17,36 +16,35 @@ await embedder.init("Xenova/clip-vit-base-patch32");
 
 const queryImages = async (imagePath: string) => {
   const queryEmbedding = await embedder.embed(imagePath);
-  const queryResult = await index.namespace('default').query({
-      vector: queryEmbedding.values,
-      includeMetadata: true,
-      includeValues: true,
-      topK: 6
+  const queryResult = await index.namespace("default").query({
+    vector: queryEmbedding.values,
+    includeMetadata: true,
+    includeValues: true,
+    topK: 6,
   });
 
-  let localPath = imagePath;
-  if (imagePath.startsWith('http://') || imagePath.startsWith('https://')) {
-    const match = imagePath.match(/\/data\/(.+)$/);
-    if (match) {
-      localPath = `data/${match[1]}`;
-    }
-  }
   // Delete the image after querying
   try {
+    let localPath = imagePath;
+    if (imagePath.startsWith("http://") || imagePath.startsWith("https://")) {
+      const match = imagePath.match(/\/data\/(.+)$/);
+      if (match) {
+        localPath = `data/${match[1]}`;
+      }
+    }
     await unlink(localPath);
   } catch (err) {
     // Optionally handle error (e.g., log it), but don't block the response
     console.error(`Failed to delete image at ${imagePath}:`, err);
   }
-  return queryResult.matches?.map(match => {
+
+  return queryResult.matches?.map((match) => {
     const { metadata } = match;
     return {
-      src: metadata ? metadata.imagePath : '',
-      score: match.score
-    };  
+      src: metadata ? metadata.imagePath : "",
+      score: match.score,
+    };
   });
 };
 
-export {
-  queryImages
-};
+export { queryImages };
